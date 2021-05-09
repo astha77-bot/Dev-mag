93eff93985663e32e2b0f2071e2b5d8acc3e468c
astha <pinkprogrammer99@gmail.com>
2021-05-10 02:32:41 +0530

    packages

--- Flask_blog.py
+++ Flask_blog.py
@@ -1,9 +1,15 @@
 from flask import Flask,render_template, url_for,flash,redirect
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from models import User,Post
 
 from wtforms.validators import Email
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
 
 posts=[
     {
